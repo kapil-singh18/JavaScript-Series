3c439c6f5f0a70823aec3fb6400fddc73562dd45
Kapil Singh Songare <168815544+kapil-singh18@users.noreply.github.com>
2025-08-08 15:39:03 +0000

    conversionOps.js added

---
+++ 01_basics/conversionOps.js
@@ -0,0 +1,32 @@
+let score = "hello";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// conversion to NUMBER
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+
+// Conversion to BOOLEAN
+let isLoggedIn = "Jjkflksd";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "hello" => true
+
+
+// Conversion to STRING
+someNumber = 54;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
