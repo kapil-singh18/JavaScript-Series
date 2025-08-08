9cfc5dd1617144cf470daef5b0fd19389664396d
Kapil Singh Songare <168815544+kapil-singh18@users.noreply.github.com>
2025-08-08 12:04:16 +0000

    dataTypes.js added

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using node.js, not browser
+
+console.log(3 + 3); console.log("hitesh"); // code readability should be high
+
+ let name = "Aden";
+ let  age = 18;
+ let isLoggedIn = false;
+ let state;
+
+ // number => 2^53 range
+ // bigint
+ // string => " "
+ // boolean => true/false
+ // null => standalone value
+ // undefiner => 
+ // symbol => uniqueness
+
+ // object
+
+ console.log(typeof undefined); // undefined
+
+ console.log(typeof null); // object
